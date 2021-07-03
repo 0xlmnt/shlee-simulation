b6efa17229f7578d373183df9346a4bf2d3a09a5
0xlmnt <contact@lmntl.io>
2021-07-03 12:41:33 +0200

    daddy is not pleased bcs it crashes

---
+++ src/lib.rs
@@ -0,0 +1,87 @@
+use nalgebra as na;
+use rand::{Rng, RngCore};
+
+pub struct Simulation {
+    world: World,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng)
+        }
+    }
+
+    pub fn world(&self) -> &World {
+        &self.world
+    }
+}
+
+#[derive(Debug)]
+pub struct World {
+    animals: Vec<Animal>,
+    food: Vec<Food>,
+}
+
+impl World {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            animals: (0..40).map(|_| {
+                Animal::random(rng)
+            }).collect(),
+            food: (0..40).map(|_| {
+                Food::random(rng)
+            }).collect(),
+        }
+    }
+
+    pub fn animals(&self) -> &[Animal] {
+        &self.animals
+    }
+
+    pub fn food(&self) -> &[Food] {
+        &self.food
+    }
+}
+
+#[derive(Debug)]
+pub struct Animal {
+    pos: na::Point2<f32>,
+    velocity: na::Vector2<f32>,
+}
+
+impl Animal {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            pos: rng.gen(),
+            velocity: rng.gen(),
+        }
+    }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.pos
+    }
+
+    pub fn velocity(&self) -> na::Vector2<f32> {
+        self.velocity
+    }
+}
+
+#[derive(Debug)]
+pub struct Food {
+    pos: na::Point2<f32>,
+}
+
+impl Food {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            pos: rng.gen()
+        }
+    }
+
+    pub fn position(&self) -> na::Point2<f32> {
+        self.pos
+    }
+}
+
+
